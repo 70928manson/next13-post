ee73a9eb994cfc0312a91092bba8ef72ec596d5f
Manson <manson972@yahoo.com>
2023-09-19 21:34:45 +0800

    update postsSlice name and type

--- redux/slices/postsSlice.ts
+++ redux/slices/postsSlice.ts
@@ -1,39 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface PostState {
-    id: number,
-    title: string,
-    description: string,
+  id: number;
+  title: string;
+  description: string;
 }
 
 const initialState: PostState[] = [
-    {
-        id: 1,
-        title: "Post 1",
-        description: "Description 1"
-    }
+  {
+    id: 1,
+    title: "Post 1",
+    description: "Description 1",
+  },
 ];
 
 const postsSlice = createSlice({
-    name: "posts",
-    initialState,
-    reducers: {
-        addPosts: (state, action) => {
-            const { id, title, description } = action.payload;
-            state.push({
-                id,
-                title,
-                description,
-            });
-        },
-        deletePosts: (state, action) => {
-            const postId = action.payload;
-            return state.filter((post) => {
-                post.id === postId
-            });
-        }
-    }
-})
+  name: "posts",
+  initialState,
+  reducers: {
+    addPost: (state, action) => {
+      const { id, title, description } = action.payload;
+      state.push({
+        id,
+        title,
+        description,
+      });
+    },
+    deletePost: (state, action) => {
+      const postId = action.payload;
+      return state.filter((post: PostState) => { return post.id !== postId });
+    },
+  },
+});
 
-export const { addPosts, deletePosts } = postsSlice.actions;
+export const { addPost, deletePost } = postsSlice.actions;
 export default postsSlice.reducer;
