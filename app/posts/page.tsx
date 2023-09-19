4369d583b2e22ef6dce30da74d11bf6f447fa0e0
Manson <manson972@yahoo.com>
2023-09-19 21:51:33 +0800

    update post function

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,23 +1,82 @@
-'use client'
+"use client";
 
 import { useSelector, useDispatch } from "react-redux";
-import { addPosts, deletePosts } from "@/redux/slices/postsSlice";
-import styles from './page.module.css';
+import { addPost, deletePost } from "@/redux/slices/postsSlice";
+import styles from "./page.module.css";
 import { useState } from "react";
 
-import React from 'react'
+import React from "react";
+import { RootState } from "@/redux/store";
 
 const Posts = () => {
-    const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
+  const posts = useSelector((state: RootState) => {
+    return state.posts;
+  });
 
-    const dispatch = useDispatch();
-    const posts = useSelector((state: any) => {
-        state.posts
-    })
+  const handleAddPost = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (!title && !description) {
+        return;
+    }
 
-    return (
-        <div>Posts</div>
-    )
-}
+    const newPost = {
+        id: Date.now(),
+        title: title,
+        description: description,
+    }
 
-export default Posts
+    dispatch(addPost(newPost));
+
+    setTitle("");
+    setDescription("");
+  }
+
+  const handleRemovePost = (postId: number) => {
+    dispatch(deletePost(postId));
+  };
+
+  return (
+    <div>
+      <form className={styles.form}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          placeholder="Description"
+          value={description}
+          className={styles.input}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button className={styles.button} onClick={handleAddPost}>
+          Add New Post
+        </button>
+      </form>
+      <h1 className={styles.heading}>Posts</h1>
+      {posts ? (
+        posts.map((post: any) => (
+          <div key={post.id} className={styles.post}>
+            <h3 className={styles.title}>{post.title}</h3>
+            <p className={styles.description}>{post.description}</p>
+            <button
+              className={styles.deleteButton}
+              onClick={() => handleRemovePost(post.id)}
+            >
+              Delete
+            </button>
+          </div>
+        ))
+      ) : (
+        <p>No posts found.</p>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
--- redux/slices/postsSlice.ts
+++ redux/slices/postsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface PostState {
   id: number;
@@ -18,7 +18,18 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    addPost: (state, action) => {
+    addPosts: (state, action: PayloadAction<PostState[]>) => {
+      state.push(...action.payload);
+    },
+    updatePost: (state, action: PayloadAction<PostState>) => {
+      const { id, title, description } = action.payload;
+      const postIndex = state.findIndex((post: PostState) => post.id === id);
+      if (postIndex !== -1) {
+        state[postIndex].title = title;
+        state[postIndex].description = description;
+      }
+    },
+    addPost: (state, action: PayloadAction<PostState>) => {
       const { id, title, description } = action.payload;
       state.push({
         id,
@@ -26,7 +37,7 @@ const postsSlice = createSlice({
         description,
       });
     },
-    deletePost: (state, action) => {
+    deletePost: (state, action: PayloadAction<number>) => {
       const postId = action.payload;
       return state.filter((post: PostState) => { return post.id !== postId });
     },
