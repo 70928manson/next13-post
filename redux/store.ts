33cb81e7d49e016306c762eb70f715eafebcd45e
Manson <manson972@yahoo.com>
2023-09-19 21:34:01 +0800

    update store reducer and types

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "./slices/postsSlice";
 
 export const store = configureStore({
-    reducer: {
-        
-    }
-})
+  reducer: {
+    posts: postsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
